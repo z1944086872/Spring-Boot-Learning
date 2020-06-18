acb06211445d78cd2cdcb911af85d8f5947adf33
leo <18890395849@163.com>
2020-06-18 14:25:04 +0800

    "Hello World!" Controller

---
+++ HelloDemo/src/main/java/com/leo/main/HelloMain.java
@@ -0,0 +1,13 @@
+package com.leo.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloMain.class, args);
+    }
+
+}
